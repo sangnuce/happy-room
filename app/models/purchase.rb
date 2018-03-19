921ec969c6795fcd4dd8f2226271b2456ced1f84
Luong Van Sang <sanga2k50@gmail.com>
2018-03-19 21:43:37 +0700

    Statistic in month

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,7 +2,8 @@ class Purchase < ApplicationRecord
   belongs_to :user
 
   scope :recent, ->{order created_at: :desc}
-  scope :in_month, ->date do
+  scope :in_month, ->month do
+    date = month ? "01/#{month}".to_date : Time.zone.today
     where "date >= ? AND date <= ?", date.beginning_of_month, date.end_of_month
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :purchases, dependent: :destroy
+
+  def amount_purchased_in_month month
+    purchases.in_month(month).reduce(0){|sum, item| sum + item.amount}
+  end
 end
